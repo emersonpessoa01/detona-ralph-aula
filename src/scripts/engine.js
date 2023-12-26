b945d963d740a6c84049c048e7396cb3ad118ddd
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-12-26 19:08:07 -0300

    added listener :rocket:

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -8,6 +8,8 @@ const state = {
   values: {
     timeId: null,
     gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
   },
 };
 function randomSquare() {
@@ -17,16 +19,26 @@ function randomSquare() {
   let randomNumber = Math.floor(Math.random() * 9);
   let randomSquare = state.view.squares[randomNumber];
   randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
 }
 function moveEnemy() {
   state.view.timeId = setInterval(randomSquare, state.values.gameVelocity);
 }
 function addListenerHitBox() {
-  state.view.squares.forEach((square) => {});
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+      }
+    });
+  });
 }
 
 function initialize() {
   //   randomSquare();
   moveEnemy();
+  addListenerHitBox();
 }
 initialize();
